4fdce7fca52072fe1f01ae81c853f1b68b22b99b
Daniel Fenjves <dfenjves@users.noreply.github.com>
2016-07-27 08:51:47 -0400

    Update Contents.swift

--- AdvancedFunctionsPractice2.playground/Contents.swift
+++ AdvancedFunctionsPractice2.playground/Contents.swift
@@ -6,54 +6,22 @@ import UIKit
 
 
 //1. Min and Max
-// Complete the following function so that it returns two integer values, min and max.
+// Complete the following function so that it returns the average of three numbers
 
-//func minAndMax(num1: Int, num2: Int)->(String, Int) {
+//func minAndMax(num1: Int, num2: Int, num3: Int)->Double {
 //
 //}
-
-
 
 
 
 //-------------------------------------------------------------------------------------------------------------------------------
 //2. Converting dollars to change
-//Write a function that takes in an integer parameter that represents a number of dollars. It should return four iteger values: the number of quarters, dimes, nickles, and pennies that are equivalent in value
-//let conversion = dollarsToChange(5)
-//print(conversion.quarters) // prints 20
-//print(conversion.dimes) // prints 50
-//print(conversion.nickles) // prints 100
-//print(conversion.pennies) // prints 500
-
-
+//Write a function that takes in an Double parameter that represents a number of dollars and cents. It should return a string that shows the least number of coins needed to make up that amount.
+//dollarsToChange(5.06) // prints "20 Quarters, 0 Dimes, 1 Nickel, 1 Penny"
 
 
 
 //-------------------------------------------------------------------------------------------------------------------------------
-//3. Change Counter
-//Write a function that takes in four integer parameters representing the number of quarters, dimes, nickles, and pennies in your pocket. It will return one double value and one integer value: value, the total value of the coins, and coinCount, the total number of coins.
-//let change = changeCounter(5, dimes: 3, nickles: 0, pennies: 11)
-//print(change.value) // prints 1.66
-//print(conversion.coinCount) // prints 19
-
-
-
-
-
-//-------------------------------------------------------------------------------------------------------------------------------
-//Bonus: Complete the following function, bookInformation, so that it returns three values: title, author, and publicationYear
-//Hint: use the componentsSeparatedByString method to break up the information in the string
-
-//func bookInformation(description: String)->(title: String, author: String, publicationDate: Int) {
-//
-//}
-
-//let information = bookInformation("The Hobbit -- J. R. R. Tolkien -- 1937 ")
-//print(information.title) //this prints 'The Hobbit'
-//print(information.author) //this prints 'J. R. R. Tolkien'
-//print(information.publicationDate) //this prints 1937
-
-
 
 
 
